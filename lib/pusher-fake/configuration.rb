15221d79577da4bab9fef0a9a5cb15d99a47f7d3
Nick Morgan <nimorgan@gmail.com>
2015-10-06 22:59:28 -0400

    Making host and ports configurable

--- lib/pusher-fake/configuration.rb
+++ lib/pusher-fake/configuration.rb
@@ -12,11 +12,6 @@ module PusherFake
     # @return [String] The Pusher API token. (Defaults to +PUSHER_API_SECRET+.)
     attr_accessor :secret
 
-    # Options for the socket server. See +EventMachine::WebSocket.start+ for options.
-    #
-    # @return [Hash] Options for the socket server.
-    attr_accessor :socket_options
-
     # @return [Boolean] Enable verbose logging.
     attr_accessor :verbose
 
@@ -28,17 +23,26 @@ module PusherFake
     # @return [Array] An array of webhook URLs. (Defaults to +[]+.)
     attr_accessor :webhooks
 
+    # @return [String] Hostname or ip address for socket_options and web_options
+    attr_accessor :host
+
+    # @return [Integer] Port for socket_options
+    attr_accessor :socket_port
+
+    # @return [Integer] Port for web_options
+    attr_accessor :web_port
+
     # Instantiated from {PusherFake.configuration}. Sets the defaults.
     def initialize
-      self.app_id   = "PUSHER_APP_ID"
-      self.key      = "PUSHER_API_KEY"
-      self.logger   = STDOUT.to_io
-      self.secret   = "PUSHER_API_SECRET"
-      self.verbose  = false
-      self.webhooks = []
-
-      self.socket_options = { host: "127.0.0.1", port: available_port }
-      self.web_options    = { host: "127.0.0.1", port: available_port }
+      self.app_id       = "PUSHER_APP_ID"
+      self.key          = "PUSHER_API_KEY"
+      self.logger       = STDOUT.to_io
+      self.secret       = "PUSHER_API_SECRET"
+      self.verbose      = false
+      self.webhooks     = []
+      self.host         = '127.0.0.1'
+      self.socket_port  = available_port
+      self.web_port     = available_port
     end
 
     # Convert the configuration to a hash sutiable for Pusher JS options.
@@ -49,6 +53,20 @@ module PusherFake
         wsHost: socket_options[:host],
         wsPort: socket_options[:port]
       )
+    end
+
+    # Options for the socket server. See +EventMachine::WebSocket.start+ for options.
+    #
+    # @return [Hash] Options for the socket server.
+    def socket_options
+      { host: host, port: socket_port }
+    end
+
+    # Options for the web server. See +Thin::Server+ for options.
+    #
+    # @return [Hash] Options for the web server.
+    def web_options
+      { host: host, port: web_port }
     end
 
     private
--- spec/lib/pusher-fake/configuration_spec.rb
+++ spec/lib/pusher-fake/configuration_spec.rb
@@ -7,6 +7,9 @@ describe PusherFake::Configuration do
   it { should have_configuration_option(:secret).with_default("PUSHER_API_SECRET") }
   it { should have_configuration_option(:verbose).with_default(false) }
   it { should have_configuration_option(:webhooks).with_default([]) }
+  it { should have_configuration_option(:host).with_default('127.0.0.1') }
+  it { should have_configuration_option(:socket_port) }
+  it { should have_configuration_option(:web_port) }
 
   it "should have configuration option :socket_options" do
     expect(subject.socket_options).to be_a(Hash)
@@ -18,6 +21,58 @@ describe PusherFake::Configuration do
     expect(subject.web_options).to be_a(Hash)
     expect(subject.web_options[:host]).to eq("127.0.0.1")
     expect(subject.web_options[:port]).to be_a(Integer)
+  end
+
+  context 'when the host is set' do
+    let(:host) { '192.168.1.1' }
+
+    before do
+      subject.host = host
+    end
+
+    it 'sets the host' do
+      expect(subject.host).to eq host
+    end
+
+    it 'sets the host for socket_options' do
+      expect(subject.socket_options[:host]).to eq host
+    end
+
+    it 'sets the host for web_options' do
+      expect(subject.web_options[:host]).to eq host
+    end
+  end
+
+  context 'when the web_port is set' do
+    let(:web_port) { 4444 }
+
+    before do
+      subject.web_port = web_port
+    end
+
+    it 'sets the web_port' do
+      expect(subject.web_port).to eq web_port
+    end
+
+    it 'sets the port for web_options' do
+      expect(subject.web_options[:port]).to eq web_port
+    end
+  end
+
+  context 'when the socket_port is set' do
+    let(:socket_port) { 4444 }
+
+    before do
+      subject.socket_port = socket_port
+    end
+
+    it 'sets the socket_port' do
+      expect(subject.socket_port).to eq socket_port
+    end
+
+    it 'sets the port for socket_options' do
+      expect(subject.socket_options[:port]).to eq socket_port
+    end
   end
 
   it "defaults socket and web ports to different values" do
